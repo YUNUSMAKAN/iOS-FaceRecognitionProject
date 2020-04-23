46d7d618ba5f4aa22700eed202d3f690930c131d
Yunus MAKAN <makanyunus@gmail.com>
2020-04-23 16:48:03 +0300

    Add Files

--- FaceRecognitionProject/ViewController.swift
+++ FaceRecognitionProject/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signInClicked(_ sender: Any) {
+        
+        let authContext = LAContext()
+        
+        var error : NSError?
+        if authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
+            authContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Is it you?") { (success, error) in
+                if success == true {
+                    //successfulwith
+                    DispatchQueue.main.async {
+                        
+                self.performSegue(withIdentifier: "to2VC", sender: nil)
+        }
+                }else{
+                    DispatchQueue.main.async {
+                        
+                    
+                        self.resultLabel.text = "ERROR!"
+                        
+                    }
+                }
+            }
+            
+            
+            
+        }
+    }
+    
 }
 
