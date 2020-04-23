671d28b0e86dcd522e16161b8003893e4bcf9d06
Yunus MAKAN <makanyunus@gmail.com>
2020-04-23 13:09:02 +0300

    Initial Commit

---
+++ FaceRecognitionProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceRecognitionProject
+//
+//  Created by MAKAN on 23.04.2020.
+//  Copyright © 2020 YUNUS MAKAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
